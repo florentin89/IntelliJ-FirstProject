1a6214b4d0e51b3c73d07a1fb6a1b714b7c3ed63
Florentin <lupascu_florentin@yahoo.com>
2019-10-04 11:25:20 +0100

    Initial commit: Created first Class in intelliJ.

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String[] orase = {"Cluj-Napoca", "Londra", "Suceava"};
+
+
+
+        System.out.println("Hello Mihai !");
+    }
+}
